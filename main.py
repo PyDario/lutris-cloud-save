f2c7a5352f64413cf009a4f477bf029fc0f4d50d
PyDario <dario@pichowsky.de>
2022-09-25 15:13:47 +0200

    resolve placeholders

--- main.py
+++ main.py
@@ -4,11 +4,19 @@ import os
 game_name = os.environ.get("game_name")
 cloudsave_folder = os.environ.get("CLOUDSAVE_FOLDER")
 
+placeholders = {
+    "%LOCALAPPDATA%": os.environ.get("LOCALAPPDATA")
+}
+
 with open("./lutris-save-file-locations.yml/lutris-save-file-locations.yml", "r") as stream:
     save_file_location = ''
     try: 
         save_file_location = yaml.safe_load(stream)["Downwell"]
     except yaml.YAMLError as exc:
         print(exc)
+
+# Resolve placeholders
+for placeholder in placeholders:
+    save_file_location = save_file_location.replace(placeholder, placeholders[placeholder])
 
 os.system("./cloudsend.sh/cloudsend.sh -e '" + save_file_location + "' " + cloudsave_folder)
