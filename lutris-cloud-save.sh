c961e84e5e6fe0d7fc83189f4e0e9cbe31767625
PyDario <dario@pichowsky.de>
2022-09-25 14:16:24 +0200

    offer environment variables

--- lutris-cloud-save.sh
+++ lutris-cloud-save.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Shared folder for your cloud saves
+# CLOUDSAVE_FOLDER=
+# Enter the Password to your Shared Nextcloud folder here
+# CLOUDSEND_PASSWORD= 
+
+# Resolve Placeholders
 LOCALAPPDATA=$WINE_PREFIX/drive_c/users/$USER/AppData
 
 pip install pyyaml
