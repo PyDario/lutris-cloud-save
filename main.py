120203aad374be9c58185440482f98e20370ec4e
PyDario <dario@pichowsky.de>
2022-09-25 15:50:10 +0200

    remove hard coded game

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ placeholders = {
 with open("./lutris-save-file-locations.yml/lutris-save-file-locations.yml", "r") as stream:
     save_file_location = ''
     try: 
-        save_file_location = yaml.safe_load(stream)["Downwell"]
+        save_file_location = yaml.safe_load(stream)[game_name]
     except yaml.YAMLError as exc:
         print(exc)
 
@@ -19,4 +19,4 @@ with open("./lutris-save-file-locations.yml/lutris-save-file-locations.yml", "r"
 for placeholder in placeholders:
     save_file_location = save_file_location.replace(placeholder, placeholders[placeholder])
 
-os.system("./cloudsend.sh/cloudsend.sh -e '" + save_file_location + "' " + cloudsave_folder)
+os.system("./cloudsend.sh/cloudsend.sh -e '" + save_file_location + "' '" + cloudsave_folder + "'")
