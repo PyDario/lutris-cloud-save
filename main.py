f0231cd255b58305cf1d1d115c2157eca75edcaa
PyDario <dario@pichowsky.de>
2022-09-25 14:16:53 +0200

    Test out save file upload with Downwell

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import yaml
 import os
 
 game_name = os.environ.get("game_name")
+cloudsave_folder = os.environ.get("CLOUDSAVE_FOLDER")
+
 with open("./lutris-save-file-locations.yml/lutris-save-file-locations.yml", "r") as stream:
     save_file_location = ''
     try: 
@@ -9,4 +11,4 @@ with open("./lutris-save-file-locations.yml/lutris-save-file-locations.yml", "r"
     except yaml.YAMLError as exc:
         print(exc)
 
-print(save_file_location)
+os.system("./cloudsend.sh/cloudsend.sh -e '" + save_file_location + "' " + cloudsave_folder)
