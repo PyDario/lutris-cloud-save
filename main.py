d236aa7c691f084c0409919ded5e252a5f974d49
PyDario <dario@pichowsky.de>
2023-01-08 20:22:44 +0100

    if load mode then load save files from cloud

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import yaml
 import logging
 import pysftp
 
+is_load_mode = True
 game_name = os.environ.get("game_name")
 if game_name == None:
     logging.error("game_name is not set. Check if the file was started from a lutris runtime")
@@ -46,4 +47,8 @@ for placeholder in placeholders:
     save_file_location = save_file_location.replace(placeholder, placeholders[placeholder])
 
 with pysftp.Connection(ftp_hostname, username=ftp_user, password=ftp_password) as sftp:
-    sftp.put_r(save_file_location, ftp_save_folder+"/"+game_name, preserve_mtime=True)
+    if not is_load_mode:
+        sftp.put_r(save_file_location, ftp_save_folder+"/"+game_name, preserve_mtime=True)
+    else:
+        print (ftp_save_folder+"/"+game_name)
+        sftp.get_d(ftp_save_folder+"/"+game_name, save_file_location, preserve_mtime=True)
